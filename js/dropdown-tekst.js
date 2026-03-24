9659d466fa1b484fb20a1d1945d311b64acce145
JennyWithChuu <Larsenjeanne004@gmail.com>
2026-03-24 12:06:16 +0100

    Dropdown

--- js/dropdown-tekst.js
+++ js/dropdown-tekst.js
@@ -1,1 +1,21 @@
-// hej
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach(function(dropdown) {
+
+    const header = dropdown.querySelector(".dropdown-header");
+
+    dropdown.addEventListener("click", function() {
+        const isOpen = dropdown.classList.contains("active");
+
+        dropdowns.forEach(function(item) {
+            item.classList.remove("active");
+        });
+
+        if (!isOpen) {
+            dropdown.classList.add("active");
+        }
+
+        });
+    });
+
+    
