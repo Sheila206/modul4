89762d1d35e6bf6890da5e51b19a8943e1cf063e
adnat38 <adnat38@gmail.com>
2026-03-24 09:18:23 +0100

    login js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,29 @@
-// hej
+function login() {
+    let username = document.getElementById("brugernavn").value;
+    let password = document.getElementById("adgangskode").value;
+    let message = document.getElementById("message");
+
+    let correctUsername = "SofieHansen";
+    let correctPassword = "12345";
+
+    /* Rydder gammel tekst */
+    message.innerText = "";
+
+    if (username === correctUsername && password === correctPassword) {
+        message.innerText = "Du er logget ind!";
+        message.style.color = "green";
+
+        setTimeout(() => {
+            window.location.href = "main.html";
+        }, 1000);
+
+    } else {
+        message.innerText = "Invalid code or username";
+        message.style.color = "red";
+    }
+
+        setTimeout(() => {
+        message.innerText = "";
+    }, 2000);
+}
+
